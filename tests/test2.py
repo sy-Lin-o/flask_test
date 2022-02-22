b1cb739bf2ba4e862abd277dfe461ef914faf289
lsy <linsy_0908@163.com>
2022-02-22 23:40:51 +0800

    add test5

--- tests/test2.py
+++ tests/test2.py
@@ -2,6 +2,8 @@ from flask import render_template
 from flask import Flask
 from flask_api import app
 from flask import request
+import logging
+from logging.config import dictConfig
 
 with app.test_request_context('/hello', method='POST'):
     # now you can do something with the request until the
